63d8ff22e23b8e15a319e598d8edb914718b3829
therealmitchconnors <mitchconnors@gmail.com>
2022-05-02 14:34:51 -0700

    Fixes #1 and adds tests

---
+++ src/functions.js
@@ -0,0 +1,68 @@
+import fs from 'fs';
+import download from 'download';
+import tar from 'tar';
+import {Octokit} from '@octokit/rest';
+import semver from 'semver';
+import os from 'os';
+import util from 'util';
+
+export async function getIstioRelease(expr, osvar, arch) {
+  const octokit = new Octokit()
+  let result = await octokit.rest.repos.listReleases({
+    owner: 'istio',
+    repo: 'istio',
+  })
+  const relmap = new Map();
+  result.data.forEach( rel => relmap.set(rel.tag_name, rel));
+  let max = semver.parse(semver.maxSatisfying(Array.from(relmap.keys()), expr));
+  let artifacts = relmap.get(max.raw).assets.reduce(function(map,obj) {
+    map.set(obj.name, obj);
+    return map;
+  }, new Map());
+  let extension = '';
+  if (arch === "-local") {
+    switch(os.arch()){
+      case 'arm':
+        arch = "-armv7"
+        break;
+      case 'arm64':
+        break;
+      default:
+        arch = "-amd64"
+    }
+  }
+  if (osvar === "local") {
+    switch(os.platform()) {
+      case 'darwin':
+      case 'osx':
+        osvar = "osx";
+        extension = '.tar.gz'
+        if (arch !== '-arm64') {
+          arch = ''
+        }
+        break;
+      case 'win32':
+        osvar = "win"
+        extension = '.zip'
+        break;
+      default:
+        osvar = "linux"
+        extension = '.tar.gz'
+        break;
+    }
+  }
+  let istioctlkey = util.format("%s-%s-%s%s%s", "istioctl", max.raw, osvar, arch, extension)
+  let istiokey = util.format("%s-%s-%s%s%s", "istio", max.raw, osvar, arch, extension)
+  return [max, artifacts.get(istioctlkey).browser_download_url, artifacts.get(istiokey).browser_download_url]
+}
+
+export async function downloadIstioctl(uri) {
+  const localFile = 'istioctl.tar.gz'
+  // download the tar
+  fs.writeFileSync(localFile, await download(uri));
+  // extract
+  await tar.x( { file: localFile } )
+  // delete the tar
+  fs.unlinkSync(localFile)
+}
+
---
+++ test/test.js
@@ -0,0 +1,25 @@
+import AsyncTestUtil from 'async-test-util'
+import {downloadIstioctl, getIstioRelease} from'../src/functions.js'
+import assert from 'assert'
+import validUrl from 'valid-url'
+import fs from 'fs';
+
+it('should wait until server is online', async function() {
+  this.timeout(10000)
+  var max, istioctlkey, istiokey
+  const doGetRelease = async() => {
+    [max, istioctlkey, istiokey] = await getIstioRelease('1.11.*', 'local', 'local')
+    await downloadIstioctl(istioctlkey)
+    assert.equal(max.version, "1.11.8")
+    assert.ok(validUrl.isUri(istiokey))
+    assert.ok(validUrl.isUri(istioctlkey))
+    fs.accessSync('istioctl', fs.constants.R_OK | fs.constants.X_OK);
+    // cleanup
+    fs.unlinkSync("istioctl")
+    return true
+  }
+  await AsyncTestUtil.waitUntil(doGetRelease, 10000)
+
+  // TODO: add assertions and cleanup for istioctl file
+
+});
